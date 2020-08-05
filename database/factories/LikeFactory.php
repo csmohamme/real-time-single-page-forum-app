78aa808d8d798e3af9986f771fc7c6795fee3e75
Mohammed <58958280+csmohamme@users.noreply.github.com>
2020-08-05 18:26:28 +0200

    Factory and seeds

--- database/factories/LikeFactory.php
+++ database/factories/LikeFactory.php
@@ -1,9 +1,16 @@
 <?php
 
+use App\Model\Reply;
+use App\User;
 use Faker\Generator as Faker;
 
 $factory->define(App\Model\Like::class, function (Faker $faker) {
     return [
-        //
+        'reply_id'=> function(){
+            return Reply::all()->random();
+        },
+        'user_id'=> function(){
+            return User::all()->random();
+        }
     ];
 });
