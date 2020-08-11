232fc0d26e22e862e222c9b73530b345dd8f0a33
Mohammed <58958280+csmohamme@users.noreply.github.com>
2020-08-11 15:23:04 +0200

    JWT Packeg

--- routes/api.php
+++ routes/api.php
@@ -10,3 +10,17 @@ Route::apiResource('question/{question}/reply', 'ReplyController');
 Route::post('like/{reply}', 'LikeController@Likeit');
 
 Route::delete('like/{reply}', 'LikeController@unLikeit');
+
+Route::group([
+
+    'middleware' => 'api',
+    'prefix' => 'auth'
+
+], function ($router) {
+
+    Route::post('login', 'AuthController@login');
+    Route::post('logout', 'AuthController@logout');
+    Route::post('refresh', 'AuthController@refresh');
+    Route::post('me', 'AuthController@me');
+
+});
